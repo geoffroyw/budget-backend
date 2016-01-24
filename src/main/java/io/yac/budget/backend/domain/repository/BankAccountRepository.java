318e73c82111e5b692bc73b9c25136cb0f77badb
Geoffroy Weyer <webweyer@gmail.com>
2016-01-24 10:03:40 +0100

    Bootstrap rest repositories

--- src/main/java/io/yac/budget/backend/domain/repository/BankAccountRepository.java
+++ src/main/java/io/yac/budget/backend/domain/repository/BankAccountRepository.java
@@ -1,11 +1,13 @@
 package io.yac.budget.backend.domain.repository;
 
 import io.yac.budget.backend.domain.entity.BankAccount;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  * Created by geoffroy on 23/01/2016.
  */
-public interface BankAccountRepository extends CrudRepository<BankAccount, Long> {
+@RepositoryRestResource(collectionResourceRel = "bank-accounts", path = "bank-accounts")
+public interface BankAccountRepository extends PagingAndSortingRepository<BankAccount, Long> {
 
 }
