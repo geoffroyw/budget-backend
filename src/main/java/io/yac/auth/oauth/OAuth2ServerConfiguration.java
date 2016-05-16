1251637d432856c72bec45961b6c060482339040
Geoffroy Weyer <webweyer@gmail.com>
2016-05-16 17:28:03 +0200

    secure actuator endpoints

--- src/main/java/io/yac/auth/oauth/OAuth2ServerConfiguration.java
+++ src/main/java/io/yac/auth/oauth/OAuth2ServerConfiguration.java
@@ -44,6 +44,7 @@ public class OAuth2ServerConfiguration {
         public void configure(HttpSecurity security) throws Exception {
             security.authorizeRequests().antMatchers("/users").permitAll();
             security.authorizeRequests().antMatchers("/api/**").authenticated();
+            security.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN");
         }
     }
 
