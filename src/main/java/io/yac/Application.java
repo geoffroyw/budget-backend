ad8c3aa88a8a0b1617bfdd62663486cef4e8f630
Geoffroy Weyer <webweyer@gmail.com>
2016-10-15 18:27:05 +0200

    Removes Hystrix dependency

--- src/main/java/io/yac/Application.java
+++ src/main/java/io/yac/Application.java
@@ -2,11 +2,9 @@ package io.yac;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 
 @SpringBootApplication
-@EnableHystrix
 public class Application {
 
     public static void main(String[] args) {
